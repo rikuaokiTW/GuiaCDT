79badf696296a42a32e7a53ac27bcbcb4ed77431
rikuaokiTW <riku_aoki@gmail.com>
2023-01-23 10:04:05 -0300

    Finalizando Frame de Entrada de Dados

--- refactoring.py
+++ refactoring.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 from tkinter.tix import *
 import re, os
 
@@ -8,16 +9,25 @@ class FileVerification():
 
     def isImage(self, pathImage):
         if os.path.isfile(pathImage):
-            print(pathImage)
-            print(os.path.isfile(pathImage))
             extension = self.extensionRegex.search(pathImage).group()
             if extension == 'png':
-                print("Passou aqui")
                 return True
             else:
                 return False
 
-
+    def isFont(self, pathFont):
+        if os.path.isfile(pathFont):
+            extension = self.extensionRegex.search(pathFont).group()
+            if extension == 'otf' or extension == 'ttf':
+                return True
+            else:
+                return False
+    
+    def isDirectory(self, pathDirectory):
+        if os.path.isdir(pathDirectory):
+            return True
+        else:
+            return False
 
 class VisualWarning(Label, Balloon):
     def __init__(self, master, pathImage, row, column):
@@ -29,6 +39,7 @@ class VisualWarning(Label, Balloon):
 
         self.__warning = Label(self.__master, image=self.__image)
         self.__warning.grid(row=self._row, column=self._column)
+        #self.__warning.columnconfigure(self._column, weight=0)
         
         self.__balloonWarning = Balloon(self.__master)
         self.__balloonWarning.bind_widget(self.__warning, balloonmsg="Path não identificado")
@@ -51,27 +62,152 @@ class Title(Label):
         self._columnspan = columnspan
         self.sticky = sticky
 
-        self.Label = Label(self.__master, text=self.name)
-        self.Label.grid(row=self._row, column=self._column, columnspan=self._columnspan, sticky=self.sticky)
-        
+        self.label = Label(self.__master, text=self.name)
+        self.label.grid(row=self._row, column=self._column, columnspan=self._columnspan, sticky=EW)
+        self.label.bind("<Configure>", self.setSize)
+        #self.label.columnconfigure(self._column, weight=2)
+    
+    def setSize(self, event):
+        if event.width < 70:
+            font_size = int(event.width/7)
+            #print("caso 1: ", font_size, event.width, self.label.cget("text"))
+        elif event.width >= 70 and event.width < 80:
+            font_size = int(event.width/8)
+            #print("caso 2: ", font_size, event.width, self.label.cget("text"))
+        elif event.width >= 80 and event.width < 90:
+            font_size = int(event.width/9)
+            #print("caso 3: ", font_size, event.width, self.label.cget("text"))
+        elif event.width >= 90 and event.width < 110:
+            font_size = int(event.width/10)
+            #print("caso 4: ", font_size, event.width, self.label.cget("text"))
+        elif event.width >= 110 and event.width < 150:
+            font_size = int(event.width/11)
+            #print("caso 5: ", font_size, event.width, self.label.cget("text"))
+        elif event.width >= 150:
+            font_size = 14
+            #print("caso 6: ", font_size, event.width, self.label.cget("text"))
+        self.label.configure(font=("Arial", font_size))
+
 
 class Input(Entry):
-    def __init__(self, master, width, row, column):
+    def __init__(self, master, width, row, column, textDefault = ''):
         super().__init__()
         self.__master = master
-        self.width = width
+        self._width = width
         self._row = row
         self._column = column
         self.verification = FileVerification()
         
-        self.input = Entry(self.__master, width=self.width)
-        self.input.grid(row=self._row, column=self._column)
+        self.input = Entry(self.__master, width=self._width)
+        self.input.grid(row=self._row, column=self._column, sticky=EW)
+        #self.input.columnconfigure(self._column, weight=3)
+        self.setText(textDefault)
 
     def getValue(self):
         return self.input.get()
 
+    def setText(self, text):
+        self.clearText()
+        self.input.insert(0, text)
+    
+    def clearText(self):
+        self.input.delete(0, END)
+
+class SearchButton(Button):
+    def __init__(self, master, text, padx, pady, inputIndex, row, column,bgColor='lightgray', bgActive="gray"):
+        super().__init__()
+        self.__master = master
+        self.row = row
+        self.column = column
+        self.text = text
+        self.padx = padx
+        self.pady = pady
+
+        self.searchButton = Button(self.__master, text=self.text, bg=bgColor, activebackground=bgActive, padx=self.padx, pady=self.pady, command=lambda: inputs[inputIndex]['input'].setText(self.getFileName()))
+        self.searchButton.grid(row=self.row, column=self.column)
+        #self.searchButton.columnconfigure(self.column, weight=1)
+    
+    def getFileName(self):
+        filename = filedialog.askopenfilename(initialdir=os.getcwd(),filetypes = (("*ttf, *otf, *png files",".png .otf .ttf"),("all files","*.*")))
+
+        return filename
+
+class ActionButton(Button):
+    def __init__(self, master, text, bgColor, fontColor, action, row, column, sticky=EW, span=1, listInputs=[]):
+        self.__master = master
+        self._action = action
+        self.listInputs = listInputs
+
+        self.actionButton = Button(self.__master, text=text, background=bgColor, fg=fontColor, font=("Consolas"), bd=2)
+        self.setAction()
+        self.actionButton.grid(row=row, column=column, columnspan=span, sticky=sticky)
+        self.actionButton.grid_columnconfigure(column, weight=0)
+
+    def setAction(self):
+        if self._action == 'validar':
+            self.actionButton.configure(command=lambda: self.verifyInputs())
+        elif self._action == 'limpar':
+            self.actionButton.configure(command=lambda: self.clearInputs())
+
+    def verifyInputs(self):
+        if self.listInputs:
+            for item in self.listInputs:
+                if item['type'] == 'image':
+                    if item["input"].getValue():
+                        if item["input"].verification.isImage(item["input"].getValue()):
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\accept.png")))
+                            item["warning"].setMessage("Imagem encontrada!")
+                        elif item["input"].verification.isImage(item["input"].getValue()) == None:
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                            item["warning"].setMessage("Esperava algo como C:/path/image.png")
+                        elif not item["input"].verification.isImage(item["input"].getValue()):
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                            item["warning"].setMessage("A imagem deve ser .png")
+                    else:
+                        item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                        item["warning"].setMessage("Path não identificado")
+
+                elif item['type'] == 'font':
+                    if item["input"].getValue():
+                        if item["input"].verification.isFont(item["input"].getValue()):
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\accept.png")))
+                            item["warning"].setMessage("Fonte encontrada!")
+                        elif item["input"].verification.isFont(item["input"].getValue()) == None:
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                            item["warning"].setMessage("Esperava algo como C:/path/fonte.ttf")
+                        elif not item["input"].verification.isFont(item["input"].getValue()):
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                            item["warning"].setMessage("A fonte deve ser .ttf ou .otf")
+                    else:
+                        item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                        item["warning"].setMessage("Path não identificado")
+
+                elif item['type'] == 'directory':
+                    if item["input"].getValue():
+                        if item["input"].verification.isDirectory(item["input"].getValue()):
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\accept.png")))
+                            item["warning"].setMessage("Pasta encontrada!")
+                        elif item["input"].verification.isDirectory(item["input"].getValue()) == None:
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                            item["warning"].setMessage("Esperava algo como C:/path/pasta/")
+                        elif not item["input"].verification.isDirectory(item["input"].getValue()):
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                            item["warning"].setMessage("Não é uma pasta")
+                            
+                    else:
+                        item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                        item["warning"].setMessage("Path não identificado")
+        else:
+            pass
+    
+    def clearInputs(self):
+        for item in self.listInputs:
+            item['input'].clearText()
+
+#--------------------------------------------------------------------------
+# Início
 root = Tk()
-root.title("Testando")
+root.title("Automatizando o Guia")
 
 #--------------------------------------------------------------------------
 # Dimensao da janela do aplicativo
@@ -80,111 +216,57 @@ dj = (500, 300)
 rm = (root.winfo_screenwidth(), root.winfo_screenheight())
 # Posicao da Janela do Aplicativo
 pj = (rm[0]/2 - dj[0]/2, rm[1]/2 - dj[1]/2)
-
+# Centralizando a Janela com relação ao monitor
 root.geometry("{}x{}+{}+{}".format(dj[0], dj[1], int(pj[0]), int(pj[1])))
 #---------------------------------------------------------------------------
-path_frame = Frame(root, width=dj[0], padx=5, pady=5, bd=3, relief="groove")
+# Frame de Entrada de Dados
+input_frame = Frame(root, width=dj[0], padx=5, pady=5, bd=3, relief="groove")
 
-# Definindo Rótulos
+# Colocando Entrada de Paths de Arquivos
 inputs = [
     {
-        "label":Title(path_frame, "Imagem Base:", 0, 1),
-        "input":Input(path_frame, "50", 0, 2),
-        "warning":VisualWarning(path_frame, r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png', 0, 0),
+        "type": 'image', 
+        "label":Title(input_frame, "Imagem Base:", 0, 1),
+        "input":Input(input_frame, "50", 0, 2, r'{}'.format(os.path.join(os.getcwd(), r"src\images\test-cdt.png"))),
+        "warning":VisualWarning(input_frame, r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")), 0, 0),
+        "button":SearchButton(input_frame, "...", 4, 2, 0, 0, 3),
     },
     {
-        "label":Title(path_frame, "Fonte Principal:", 1, 1),
-        "input":Input(path_frame, "50", 1, 2),
-        "warning":VisualWarning(path_frame, r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png', 1, 0),
+        "type": 'font',
+        "label":Title(input_frame, "Fonte Principal:", 1, 1),
+        "input":Input(input_frame, "50", 1, 2),
+        "warning":VisualWarning(input_frame, r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")), 1, 0),
+        "button":SearchButton(input_frame, "...", 4, 2, 1, 1, 3),
     },
     {
-        "label":Title(path_frame, "Fonte com Itálico:", 2, 1),
-        "input":Input(path_frame, "50", 2, 2),
-        "warning":VisualWarning(path_frame, r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png', 2, 0),
+        "type": 'font',
+        "label":Title(input_frame, "Fonte com Itálico:", 2, 1),
+        "input":Input(input_frame, "50", 2, 2),
+        "warning":VisualWarning(input_frame, r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")), 2, 0),
+        "button":SearchButton(input_frame, "...", 4, 2, 2, 2, 3),
     },
     {
-        "label":Title(path_frame, "Local de Destino:", 3, 1),
-        "input":Input(path_frame, "50", 3, 2),
-        "warning":VisualWarning(path_frame, r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png', 3, 0),
+        "type": 'directory',
+        "label":Title(input_frame, "Local de Destino:", 3, 1),
+        "input":Input(input_frame, "50", 3, 2),
+        "warning":VisualWarning(input_frame, r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")), 3, 0),
+        "button":SearchButton(input_frame, "...", 4, 2, 3, 3, 3),
     }
 ]
-#label_base_image = Title(path_frame, "Imagem Base:", 0, 1)
-label_principal_font = Title(path_frame, "Fonte Principal:", 1, 1)
-label_italic_font = Title(path_frame, "Fonte com Itálico:", 2, 1)
-label_destiny_path = Title(path_frame, "Local de Destino:", 3, 1)
 
-# Definindo Entradas
-#input_base_image = Input(path_frame, "50", 0, 2)
-input_principal_font = Input(path_frame, "50", 1, 2)
-input_italic_font = Input(path_frame, "50", 2, 2)
-input_destiny_path = Input(path_frame, "50", 3, 2)
+# Botões
+button1 = ActionButton(input_frame, 'VALIDAR', '#7BFF78', '#0D650B', 'validar', 4, 0, span=2, listInputs=inputs)
+button2 = ActionButton(input_frame, 'APAGAR', '#FF7878', '#650B0B', 'limpar', 4, 2, span=2, listInputs=inputs)
 
-# Imagens Verificadoras
-#warning_base_image = VisualWarning(path_frame, r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png', 0, 0)
-warning_principal_font = VisualWarning(path_frame, r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png', 1, 0)
-warning_italic_font = VisualWarning(path_frame, r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png', 2, 0)
-warning_destiny_path = VisualWarning(path_frame, r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png', 3, 0)
+input_frame.grid(row=0, column=0, sticky=EW)
 
-#warning_base_image.setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/add.png')
-
-
-
-def testeDado():
-    for item in inputs:
-        if item["input"].getValue():
-            if item["input"].verification.isImage(item["input"].getValue()):
-                item["warning"].setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/accept.png')
-                item["warning"].setMessage("Imagem Encontrada!")
-            elif item["input"].verification.isImage(item["input"].getValue()) == None:
-                item["warning"].setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png')
-                item["warning"].setMessage("Esperava algo como C:/path/image.png")
-            elif not item["input"].verification.isImage(item["input"].getValue()):
-                item["warning"].setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png')
-                item["warning"].setMessage("A imagem deve ser .png")
-        else:
-            item["warning"].setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png')
-            item["warning"].setMessage("Path não identificado")
-            
-    """ for x in range(5):
-        if inputs[x].getValue():
-            if inputs[x].verification.isImage(inputs[x].getValue()):
-                warnings[x].setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/accept.png')
-                warnings[x].setMessage("Imagem Encontrada!")
-            elif inputs[x].verification.isImage(inputs[x].getValue()) == None:
-                warnings[x].setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png')
-                warnings[x].setMessage("Esperava algo como C:/path/image.png")
-            elif not inputs[x].verification.isImage(inputs[x].getValue()):
-                warnings[x].setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png')
-                warnings[x].setMessage("A imagem deve ser .png")
-        else:
-            warnings[x].setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png')
-            warnings[x].setMessage("Path não identificado") """
-            
-
-    """ if input_base_image.getValue():
-        if input_base_image.verification.isImage(input_base_image.getValue()):
-            warning_base_image.setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/accept.png')
-            warning_base_image.setMessage("Imagem Encontrada!")
-        elif input_base_image.verification.isImage(input_base_image.getValue()) == None:
-            warning_base_image.setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png')
-            warning_base_image.setMessage("Esperava algo como C:/path/image.png")
-        elif not input_base_image.verification.isImage(input_base_image.getValue()):
-            warning_base_image.setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png')
-            warning_base_image.setMessage("A imagem deve ser .png")
-    else:
-        warning_base_image.setImage(r'C:/Users/Riku Aoki/Documents/Programas/GuiaCDT/src/icons/cancel.png')
-        warning_base_image.setMessage("Path não identificado") """
-
-    #print(frame01.returnText())
-
-btn2 = Button(root, text = "Show Original", command=lambda: testeDado())
-btn2.grid(row=2, column=0)
-
-path_frame.grid(row=0, column=0)
+#---------------------------------------------------------------------------
+# Frame de Preview da Imagem
 
 # Responsividade
-path_frame.grid_rowconfigure(0, weight=1)
-path_frame.grid_columnconfigure(0, weight=1)
+input_frame.grid_columnconfigure([0,3], weight=0)
+input_frame.grid_columnconfigure(1, weight=1)
+input_frame.grid_columnconfigure(2, weight=3)
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
