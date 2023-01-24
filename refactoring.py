1554f18022a6549f97325d4bfeec7f9a301f33f0
rikuaokiTW <riku_aoki@gmail.com>
2023-01-24 11:10:35 -0300

    Adiantando área de preview e de ajustes finos

--- refactoring.py
+++ refactoring.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter.tix import *
+from PIL import Image, ImageDraw, ImageFont
 import re, os
 
 class FileVerification():
@@ -11,6 +12,14 @@ class FileVerification():
         if os.path.isfile(pathImage):
             extension = self.extensionRegex.search(pathImage).group()
             if extension == 'png':
+                return True
+            else:
+                return False
+
+    def isText(self, pathText):
+        if os.path.isfile(pathText):
+            extension = self.extensionRegex.search(pathText).group()
+            if extension == 'txt':
                 return True
             else:
                 return False
@@ -128,17 +137,17 @@ class SearchButton(Button):
         #self.searchButton.columnconfigure(self.column, weight=1)
     
     def getFileName(self):
-        filename = filedialog.askopenfilename(initialdir=os.getcwd(),filetypes = (("*ttf, *otf, *png files",".png .otf .ttf"),("all files","*.*")))
+        filename = filedialog.askopenfilename(initialdir=os.getcwd(),filetypes = (("*ttf, *otf, *txt, *png files",".txt .png .otf .ttf"),("all files","*.*")))
 
         return filename
 
 class ActionButton(Button):
-    def __init__(self, master, text, bgColor, fontColor, action, row, column, sticky=EW, span=1, listInputs=[]):
+    def __init__(self, master, text, bgColor, fontColor, action, row, column, sticky=N, span=1, listInputs=[]):
         self.__master = master
         self._action = action
         self.listInputs = listInputs
 
-        self.actionButton = Button(self.__master, text=text, background=bgColor, fg=fontColor, font=("Consolas"), bd=2)
+        self.actionButton = Button(self.__master, text=text, background=bgColor, fg=fontColor, width="40", font=("Consolas"), bd=2)
         self.setAction()
         self.actionButton.grid(row=row, column=column, columnspan=span, sticky=sticky)
         self.actionButton.grid_columnconfigure(column, weight=0)
@@ -193,6 +202,18 @@ class ActionButton(Button):
                         elif not item["input"].verification.isDirectory(item["input"].getValue()):
                             item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
                             item["warning"].setMessage("Não é uma pasta")
+
+                elif item['type'] == 'text':
+                    if item["input"].getValue():
+                        if item["input"].verification.isText(item["input"].getValue()):
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\accept.png")))
+                            item["warning"].setMessage("Texto encontrado!")
+                        elif item["input"].verification.isText(item["input"].getValue()) == None:
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                            item["warning"].setMessage("Esperava algo como C:/path/texto.txt")
+                        elif not item["input"].verification.isText(item["input"].getValue()):
+                            item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
+                            item["warning"].setMessage("Deve ser um arquivo .txt")
                             
                     else:
                         item["warning"].setImage(r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")))
@@ -219,7 +240,7 @@ pj = (rm[0]/2 - dj[0]/2, rm[1]/2 - dj[1]/2)
 # Centralizando a Janela com relação ao monitor
 root.geometry("{}x{}+{}+{}".format(dj[0], dj[1], int(pj[0]), int(pj[1])))
 #---------------------------------------------------------------------------
-# Frame de Entrada de Dados
+# Definindo Frame de Entrada de Dados
 input_frame = Frame(root, width=dj[0], padx=5, pady=5, bd=3, relief="groove")
 
 # Colocando Entrada de Paths de Arquivos
@@ -251,22 +272,169 @@ inputs = [
         "input":Input(input_frame, "50", 3, 2),
         "warning":VisualWarning(input_frame, r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")), 3, 0),
         "button":SearchButton(input_frame, "...", 4, 2, 3, 3, 3),
-    }
+    },
+    {
+        "type": 'text',
+        "label":Title(input_frame, "Info Animes:", 4, 1),
+        "input":Input(input_frame, "50", 4, 2),
+        "warning":VisualWarning(input_frame, r'{}'.format(os.path.join(os.getcwd(), r"src\icons\cancel.png")), 4, 0),
+        "button":SearchButton(input_frame, "...", 4, 2, 4, 4, 3),
+    },
 ]
 
 # Botões
-button1 = ActionButton(input_frame, 'VALIDAR', '#7BFF78', '#0D650B', 'validar', 4, 0, span=2, listInputs=inputs)
-button2 = ActionButton(input_frame, 'APAGAR', '#FF7878', '#650B0B', 'limpar', 4, 2, span=2, listInputs=inputs)
+button1 = ActionButton(input_frame, 'VALIDAR', '#7BFF78', '#0D650B', 'validar', 5, 0, span=2, listInputs=inputs)
+button2 = ActionButton(input_frame, 'APAGAR', '#FF7878', '#650B0B', 'limpar', 5, 2, span=2, listInputs=inputs)
 
+# Inserindo Frame de Entrada de Dados
 input_frame.grid(row=0, column=0, sticky=EW)
 
 #---------------------------------------------------------------------------
-# Frame de Preview da Imagem
+# Definindo Frame de Preview da Imagem
+preview_frame = Frame(root, width=dj[0], padx=5, pady=5, bd=3, relief="groove")
 
+# Imagem
+base_image = PhotoImage(file=os.path.join(os.getcwd(), r'src/images/preview.png'))
+image_preview = Label(preview_frame, image=base_image)
+image_preview.grid(row=0, column=0)
+
+preview_frame.grid(row=1, column=0, sticky=EW)
+
+#---------------------------------------------------------------------------
+# Definindo Frame de Textos de Teste da Imagem
+preview_texts = Frame(root, width=dj[0], padx=5, pady=5, bd=3, relief="groove")
+
+# Texto de Teste
+# Labels
+title_label = Label(preview_texts, text="1. Título:", padx=5)
+title_label.grid(row=0, column=0)
+
+# Inputs
+title_input = Entry(preview_texts, width="40")
+title_input.grid(row=0, column=1, sticky=EW, pady=10)
+
+preview_texts.grid(row=2, column=0, sticky=EW)
+
+#---------------------------------------------------------------------------
+# Layout de Ajuste finos
+adjust_canvas = Canvas(root, width=dj[0])
+adjust_frame = Frame(adjust_canvas)
+
+# Criando Labels
+title_label = Label(adjust_frame, text="Título")
+title_label.grid(row=0, column=0)
+
+# Frame dos inputs
+titles_frame = Frame(adjust_frame, bg="gray", padx=10, pady=10, bd=3)
+
+
+# Criando label das coordenadas
+font_height_label = Label(titles_frame, text="font size")
+font_height_label.grid(row=0, column=0)
+coordx_label = Label(titles_frame, text="coordx")
+coordx_label.grid(row=1, column=0)
+coordy_label = Label(titles_frame, text="coordy")
+coordy_label.grid(row=2, column=0)
+
+# Criando inputs dos titulos
+font_height_input = Entry(titles_frame)
+font_height_input.grid(row=0, column=1)
+coordx_input = Entry(titles_frame)
+coordx_input.grid(row=1, column=1)
+coordy_input = Entry(titles_frame)
+coordy_input.grid(row=2, column=1)
+
+# Fechando Frame dos Inputs
+titles_frame.grid(row=1, column=0)
+
+# Barra de Scroll
+scrollbar = Scrollbar(root, orient='vertical')
+scrollbar.grid(row=0, column=1, sticky=NS)
+
+# Configurando o comando da scrollbar para controlar o scroll do canva
+scrollbar.config(command = adjust_canvas.yview)
+
+# Configurando scroll pra controlar a área vertical
+#root.config(yscrollcommand=scrollbar.set)
+adjust_canvas['yscrollcommand'] = scrollbar.set
+
+# Conectar a barra de rolagem ao evento/área que você quer rolar
+adjust_canvas.bind('<Configure>', lambda event: adjust_canvas.configure(scrollregion = adjust_canvas.bbox("all")))
+
+# Adicionando o adjust_frame ao canva para habilitar o scroll
+adjust_canvas.create_window((0,0), window=adjust_frame, anchor=NE)
+
+adjust_canvas.grid(row=3, column=0, sticky=EW)
+
+#---------------------------------------------------------------------------
+# Acionar Preview
+def add_it():
+    # Abrindo a imagem
+    preview_image = Image.open(os.path.join(os.getcwd(), r'src/images/preview.png'))
+    
+    # Pegando o texto pra adicionar
+    text_to_add = title_input.get()
+
+    # Coordenadas adaptadas pra preview
+    preview_font_height = round((int(font_height_input.get()) / 1080) * 300)
+    preview_coordx = (int(coordx_input.get()) / 1920) * 523
+    preview_coordy = (int(coordy_input.get()) / 1080) * 300
+
+    # Definindo a fonte
+    text_font = ImageFont.truetype(os.path.join(os.getcwd(), r'src/fonts/coolvetica rg.otf'), preview_font_height)
+
+    # Editando a imagem
+    preview_image_edit = ImageDraw.Draw(preview_image)
+    preview_image_edit.text((preview_coordx, preview_coordy), text_to_add, anchor="mm", font=text_font)
+
+    # Salvando a imagem
+    preview_image.save(os.path.join(os.getcwd(), r'src/images/new_preview.png'))
+
+    # Limpe a entry box
+    title_input.delete(0, END)
+    title_input.insert(0, "Saving File...")
+
+    # Espere alguns segundos e mostre a imagem
+    image_preview.after(2000, show_pic(os.path.join(os.getcwd(), r'src/images/new_preview.png')))
+
+def show_pic(pathNewPic):
+    global new_preview
+    new_preview = PhotoImage(file=pathNewPic)
+    image_preview.config(image=new_preview)
+    title_input.delete(0, END)
+
+buttonDemo = Button(root, text="Demonstração", command=lambda: add_it())
+buttonDemo.grid(row=4, column=0, sticky=EW)
+
+
+
+""" # Barra de Scroll
+scrollbar = Scrollbar(root, orient='vertical')
+scrollbar.grid(row=0, column=1, sticky=NS)
+
+# Configurando o comando da scrollbar para controlar o scroll do canva
+scrollbar.config(command = parent_canvas.yview)
+
+# Configurando scroll pra controlar a área vertical
+#root.config(yscrollcommand=scrollbar.set)
+parent_canvas['yscrollcommand'] = scrollbar.set
+
+# Conectar a barra de rolagem ao evento/área que você quer rolar
+parent_canvas.bind('<Configure>', lambda event: parent_canvas.configure(scrollregion = parent_canvas.bbox("all")))
+
+# Adicionando o parent_frame ao canva para habilitar o scroll
+parent_canvas.create_window((0,0), window=parent_frame, anchor=NE)
+
+
+parent_canvas.grid(row=0, column=0, sticky=NSEW) 
+parent_canvas.bind("<Configure>", on_resize) """
+adjust_canvas.grid_columnconfigure(0, weight=1)
 # Responsividade
 input_frame.grid_columnconfigure([0,3], weight=0)
 input_frame.grid_columnconfigure(1, weight=1)
 input_frame.grid_columnconfigure(2, weight=3)
+input_frame.grid_rowconfigure([0,1,2,3,4], pad=10)
+preview_frame.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
